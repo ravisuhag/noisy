217ee35692971775d30c1cffcfca639e23965ac2
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-05 22:48:50 +0530

    Adding compression to express

--- config/env/default.js
+++ config/env/default.js
@@ -4,7 +4,7 @@ module.exports = {
   app: {
     title: 'MEAN.JS',
     description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
+    keywords: ['mongodb', 'express', 'angularjs', 'node.js', 'mongoose', 'passport']
   },
   port: process.env.PORT || 3000,
   templateEngine: 'swig'
