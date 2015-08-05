15cb57cc0bc1eeeb2dc5a05580ebe466d8c61062
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-05 20:52:13 +0530

    updating npm packages

--- server.js
+++ server.js
@@ -3,8 +3,16 @@
 /**
  * Module dependencies.
  */
-var config = require('../config'),
-  express = require('./express'),
-  chalk = require('chalk');
+var config = require('./config/config');
+var database = require('./lib/mongoose');
+var express = require('./lib/express');
 
+database.connect(function(db) {
+
+  // Initialize express
+  var app = express.init(db);
+  console.log('running');
+
+
+});
 
