4cd9f51bb5002976a93e97217723626d90bfdb83
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-05 22:27:39 +0530

    adding app start function and cleanup

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-var config = require('./config/config');
-var database = require('./lib/mongoose');
-var express = require('./lib/express');
 
-database.connect(function(db) {
-
-  // Initialize express
-  var app = express.init(db);
-  console.log('running');
-
-
-});
-
+var app = require('./lib/app');
+var server = app.start();
