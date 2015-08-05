5877b27e4767ed5cc6ae62d30411d4d1800982b7
Ravi Suhag <suhag.ravi@gmail.com>
2015-08-05 20:51:41 +0530

    addign config prop

---
+++ config/env/default.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  app: {
+    title: 'MEAN.JS',
+    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
+  },
+  port: process.env.PORT || 3000,
+  templateEngine: 'swig'
+};
--- config/env/development.js
+++ config/env/development.js
@@ -1,10 +1,15 @@
 'use strict';
 
 module.exports = {
-  app : {
-    title :'Noisy - Dev'
+  app: {
+    title: 'Noisy - Dev',
   },
-  server: {
-    port: 4000
+  db: {
+    uri: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/noisy-dev',
+    options: {
+      user: '',
+      pass: ''
+    },
+    debug: process.env.MONGODB_DEBUG || false // Enable mongoose debug mode
   }
 };
